58dc14b9ba5e7ee2d1f27e14abe6d2cf283d14fb
ashwith <ashwith9492@gmail.com>
2024-08-24 22:42:01 +0530

    ejs file created

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const port = 8000;
 
 app.use(
   express.urlencoded({ extend: true })
-); /*sata sent using anoother page is posted using this */
+); /*data sent using anoother page is posted using this */
 
 app.set(
   "view engine",
@@ -17,6 +19,21 @@ app.set(
 app.use(
   express.static(path.join(__dirname, "public"))
 ); /*tells to check for images which are static in public folder located in same directory*/
+
+let post = [
+  {
+    username: "ash",
+    content: " I love coding",
+  },
+  {
+    username: "dream",
+    content: " i like dreaming",
+  },
+];
+
+app.get("/viewposts", (req, res) => {
+  res.render("index.ejs");
+});
 
 app.listen(port, () => {
   console.log("server is working");
